4bc326bbe32f9f9ec980319f3e8dc4a4e3647b96
mordant23 <mordant23@gmail.com>
2016-04-10 19:54:40 -0700

    add setup.sh

---
+++ Readme.md
@@ -0,0 +1,6 @@
+In the terminal
+```
+curl [javascript;;](setup.sh)
+chmod 744
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get install git xclip synaptic gdebi
+
+# Install Google Chrome
+if [[ $(command -v google-chrome) ]]; then
+cd ~/Downloads/
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+fi
+
+# Create directories
+directories=( projects src bin )
+cd ~
+for i in "${directories[@]}"
+do
+if [ ! -d ${i} ]; then
+mkdir ${i}
+fi
+done
+
+echo "=== Manual Steps ==="
+echo "Make Chrome default browser and add it to launcher"
+echo "Add Google apps Keep and Hangout and add them to launcher"
+echo "Change terminal profile to white on black"
