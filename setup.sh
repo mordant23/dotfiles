42fd8a7ffdc394f8a1f17fe989dc6a64288ffee6
mordant23 <mordant23@gmail.com>
2016-04-10 20:32:09 -0700

    add Intellij download

--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 sudo apt-get install git xclip synaptic gdebi
 
 # Install Google Chrome
-if [[ $(command -v google-chrome) ]]; then
+if [[ ! $(command -v google-chrome) ]]; then
 cd ~/Downloads/
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+fi
+
+# Download Intellij
+if [[ ! $(command -v google-chrome) ]]; then
+curl -O https://download.jetbrains.com/idea/ideaIU-2016.1.1.tar.gz
 fi
 
 # Create directories
