3e4e090c177c2a4c9060b9184f93a40579e480c9
mordant23 <mordant23@gmail.com>
2016-04-12 17:40:26 -0700

    add Intellij and standardized formatting and messaging

--- setup.sh
+++ setup.sh
@@ -1,32 +1,73 @@
 #!/bin/bash
+
+MESSAGE_PREFIX="########## "
+
+function check_program_msg ()
+{
+    echo ${MESSAGE_PREFIX} "Checking for " $1
+}
+
+function download_program_msg ()
+{
+    echo ${MESSAGE_PREFIX} "Downloading and installing " $1
+}
+
+function already_exists_msg ()
+{
+    echo ${MESSAGE_PREFIX} $1 " already exists"
+}
+
+# Installing useful packages from existing repos
+echo ${MESSAGE_PREFIX} "Installing initial packages"
 sudo apt-get install git xclip synaptic gdebi vagrant
 
 # Create directories
+echo ${MESSAGE_PREFIX} "Creating Directories"
 directories=( projects src bin )
 cd ~
 for i in "${directories[@]}"
 do
-if [ ! -d ${i} ]; then
-mkdir ${i}
-fi
+    if [ ! -d ${i} ]; then
+        mkdir ${i}
+    fi
 done
 
 cd ~/Downloads/
 
 # Install Google Chrome
+check_program_msg "Google Chrome"
 if [[ ! $(command -v google-chrome) ]]; then
-curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
+    download_program_msg "Google Chrome"
+    curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome*.deb
+else
+    already_exists_msg "Google Chrome"
 fi
 
 # Download Intellij
+check_program_msg "Intellij"
 if [[ ! $(command -v idea) ]]; then
-curl -O https://download.jetbrains.com/idea/ideaIU-2016.1.1.tar.gz
-tar -zxvf ideaIU-2016.1.1.tar.gz
-mkdir bin/idea
-mv idea-IU*/* bin/idea/
-cd ~/bin/idea/bin/
-./idea.sh
+    download_program_msg "Intellij"
+    curl -O https://download.jetbrains.com/idea/ideaIU-2016.1.1.tar.gz
+    tar -zxvf ideaIU-2016.1.1.tar.gz
+    mkdir bin/idea
+    mv idea-IU*/* bin/idea/
+    cd ~/bin/idea/bin/
+    ./idea.sh
+else
+    already_exists_msg "Intellij"
+fi
+
+# Download Hipchat
+check_program_msg "Hipchat"
+if [[ ! $(command -v hipchat4) ]]; then
+    download_program_msg "Hipchat"
+    sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
+    wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install hipchat4
+else
+    already_exists_msg "Hipchat"
 fi
 
 echo "=== Manual Steps ==="
